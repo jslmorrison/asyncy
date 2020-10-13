894065dba92b4cdc51b33beb41950846fdaf6554
John Morrison <jslmorrison@gmail.com>
2020-10-13 21:33:51 +0100

    Create the server with only one predefined response

---
+++ server.php
@@ -0,0 +1,21 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$loop = React\EventLoop\Factory::create();
+
+$server = new React\Http\Server($loop, function (Psr\Http\Message\ServerRequestInterface $request) {
+    return new React\Http\Message\Response(
+        200,
+        array(
+            'Content-Type' => 'text/plain'
+        ),
+        "Hello World!\n"
+    );
+});
+
+$socket = new React\Socket\Server(8080, $loop);
+$server->listen($socket);
+
+$loop->run();
+
